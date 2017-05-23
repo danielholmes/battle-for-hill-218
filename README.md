54f27346048d674de0e3237c0dec90497e2747d0
Daniel Holmes <daniel@danielholmes.org>
2017-05-23 18:17:37 +1000

    General configuration and layout begin

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Battle for Hill 218
+
+[BoardGameArena](https://boardgamearena.com/) implementation
---
+++ version.php
@@ -0,0 +1,3 @@
+<?php 
+// Please do not modify this file.
+$game_version_battleforhilldhau ="999999-9999";
