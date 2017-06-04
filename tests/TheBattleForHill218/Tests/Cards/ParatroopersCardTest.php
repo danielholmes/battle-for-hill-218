2961aa4f657fc7cdad80084efc7fe5b511f0892d
Daniel Holmes <daniel@danielholmes.org>
2017-06-04 18:05:46 +1000

    Fix paratrooper card test

--- tests/TheBattleForHill218/Tests/Cards/ParatroopersCardTest.php
+++ tests/TheBattleForHill218/Tests/Cards/ParatroopersCardTest.php
@@ -27,22 +27,44 @@ class ParatroopersCardTest extends TestCase
         assertThat(
             $card->getPossiblePlacements($battlefield),
             containsInAnyOrder(
+                new Position(-2, 3),
+                new Position(-1, 3),
+                new Position(0, 3),
+                new Position(1, 3),
+                new Position(2, 3),
+
+                new Position(-2, 2),
                 new Position(-1, 2),
                 new Position(0, 2),
                 new Position(1, 2),
+                new Position(2, 2),
 
+                new Position(-2, 1),
                 new Position(-1, 1),
                 new Position(1, 1),
+                new Position(2, 1),
 
+                new Position(-2, 0),
                 new Position(-1, 0),
                 new Position(1, 0),
+                new Position(2, 0),
 
+                new Position(-2, -1),
                 new Position(-1, -1),
                 new Position(1, -1),
+                new Position(2, -1),
 
+                new Position(-2, -2),
                 new Position(-1, -2),
                 new Position(0, -2),
-                new Position(1, -2)
+                new Position(1, -2),
+                new Position(2, -2),
+
+                new Position(-2, -3),
+                new Position(-1, -3),
+                new Position(0, -3),
+                new Position(1, -3),
+                new Position(2, -3)
             )
         );
     }
