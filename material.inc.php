029484ca9ae1a6f38fcc0c1c2f066e6450172547
Daniel Holmes <daniel@danielholmes.org>
2017-07-27 22:36:46 +1000

    Update copyright

--- material.inc.php
+++ material.inc.php
@@ -2,7 +2,7 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * BattleForHillDhau implementation : © <Your name here> <Your email address here>
+ * BattleForHillDhau implementation : © <Daniel Holmes> <daniel@danielholmes.org>
  *
  * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
  * See http://en.boardgamearena.com/#!doc/Studio for more information.
